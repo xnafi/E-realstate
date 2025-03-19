3dd02d0d3c325cddcee1b023c73dac19368723b9
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-19 15:52:18 +0600

    Global css added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="text-6xl text-red-500">
+    <div className="text-6xl text-red-500 text-center">
       <h2>aaaaaaaaaaaaaaaaaaaaaaaaa</h2>
     </div>
   );
